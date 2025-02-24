9bfd05918b58d6d41456834512873c315ff4993f
Alexey <revokalloppollakover@gmail.com>
2025-02-25 00:58:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/inventorymanagementapp/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.inventorymanagementapp
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.inventorymanagementapp.ui.theme.InventoryManagementAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            InventoryManagementAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.padding(20.dp)
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+            .background(color = MaterialTheme.colorScheme.primaryContainer)
+            .padding(10.dp, 0.dp, 10.dp, 0.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Hello $!",
+        )
+
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hellgo $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    InventoryManagementAppTheme {
+        LoginScreen(
+            modifier = Modifier.padding(0.dp)
+        )
+    }
+}
